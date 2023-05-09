a056618fd5341e25fc7ccc955d347d5fbf4aec94
Nick <nsagias@gmail.com>
2023-05-09 18:42:16 -0400

    raise state up one level

--- frontend/src/components/PostItemList/index.tsx
+++ frontend/src/components/PostItemList/index.tsx
@@ -1,33 +1,15 @@
-import { useState } from "react";
-import { Post } from "../PostItem/PostItem";
 import "./PostItemList.css";
 import PostItem from "../PostItem";
+import { Post } from "../PostItem/PostItem";
 
-
-const initialPosts: Post[] = [
-  {
-    id: 1,
-    text: "Lorem ipsum",
-    avatar: "/uploads/avatar1.png",
-    username: "Test User 1"
-  },
-  {
-    id: 2,
-    text: "Lorem ipsum",
-    avatar: "/uploads/avatar2.png",
-    username: "Test User 2"
-  },
-];
-
-export default function PostItemList() {
-  const [posts, setPost] = useState<Post[]>(initialPosts);
+export default function PostItemList({ onPosts}:{ onPosts: Post[] }): JSX.Element {
 
   return (
        <div className="feed">
-          { initialPosts 
-            && Array.isArray(initialPosts) 
-            && initialPosts.length > 0 
-            && initialPosts.map((post) => (
+          { onPosts 
+            && Array.isArray(onPosts) 
+            && onPosts.length > 0 
+            && onPosts.map((post) => (
               <PostItem key={post.id} post={post}/>
             )
           )}
--- frontend/src/components/PostsContainer/index.tsx
+++ frontend/src/components/PostsContainer/index.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import PostForm from "../PostForm";
+import { Post } from "../PostItem/PostItem";
 import PostItemList from "../PostItemList";
 import "./PostsContainer.css";
 
+const initialPosts: Post[] = [
+  {
+    id: 1,
+    text: "Lorem ipsum",
+    avatar: "/uploads/avatar1.png",
+    username: "Test User 1"
+  },
+  {
+    id: 2,
+    text: "Lorem ipsum",
+    avatar: "/uploads/avatar2.png",
+    username: "Test User 2"
+  },
+];
 export default function PostsContainer() {
+
+  const [posts, setPosts] = useState<Post[]>(initialPosts);
+
   return (
     <div className="container">
       <PostForm />
-      <PostItemList />
+      <PostItemList 
+        onPosts={posts}
+      />
     </div>
   );
 }
