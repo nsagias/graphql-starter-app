ac231be8a49f070793025de0a0829a1cafd21de9
Nick <nsagias@gmail.com>
2023-05-09 16:18:27 -0400

    refactor into components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,11 @@
 import "./App.css";
-import { Post } from "./components/Post/Post";
-
-const initialPosts: Post[] = [
-  {
-    id: 1,
-    text: "Lorem ipsum",
-    avatar: "/uploads/avatar1.png",
-    username: "Test User 1"
-  },
-  {
-    id: 2,
-    text: "Lorem ipsum",
-    avatar: "/uploads/avatar2.png",
-    username: "Test User 2"
-  },
-
-];
-
+import PostItemList from "./components/PostItemList";
 
 function App() {
   return (
     <div className="App">
+      <PostItemList />
+
     </div>
   );
 }
