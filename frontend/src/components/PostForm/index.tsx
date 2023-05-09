5bf3739bd270252d3d589bcb1b0bb441e2ef8723
Nick <nsagias@gmail.com>
2023-05-09 17:17:15 -0400

    create new post form

--- frontend/src/components/PostForm/index.tsx
+++ frontend/src/components/PostForm/index.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 export default function PostForm() {
+  const [postContent, setPostContent] = useState();
+
+  const handleSubmitPost = (e: any) => {}
+
   return(
-    <h1>PostForm</h1>
+    <div className="form">
+      <form onSubmit={handleSubmitPost}>
+        <textarea 
+          value={postContent}
+          placeholder="Create New Post!"
+        />
+        <input type="submit" value="Submit"/>
+      </form>
+    </div>
   );
 }
