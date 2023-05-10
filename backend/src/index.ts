26e184dc79ea219fb41f70c313e2a1f258a3f4ae
Nick <nsagias@gmail.com>
2023-05-10 19:06:42 -0400

    add helmet

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import path from 'path';
-
+import helmet from "helmet";
+import cors from "cors";
+import compress from "compression";
 
 const app = express();
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self "],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", "'unsafe-inline'"],
+    imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+  }
+}));
 
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
 const root = path.join(__dirname, "../../");
 
