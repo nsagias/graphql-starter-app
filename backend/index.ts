e607d76584324f3c41adac85aef6371a5a876c3b
Nick <nsagias@gmail.com>
2023-05-10 18:37:31 -0400

    update for server side rendering of front end

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import path from 'path';
 
 const app = express();
+const root = path.join(__dirname, "../../");
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use("/", express.static(path.join(root, "/frontend/build")));
+app.use('/uploads', express.static(path.join(root, 'uploads')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(root, "frontend/build/index.html"));
+});
+
 app.listen(8000, () => console.log("Listening on port 8000"));
