4c713a475a8efbb321dbc3f3829c36e3ff20e384
Nick <nsagias@gmail.com>
2023-05-09 20:46:18 -0400

    add helmet

--- frontend/src/components/PostsContainer/index.tsx
+++ frontend/src/components/PostsContainer/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Helmet } from "react-helmet";
 import PostForm from "../PostForm";
 import { Post } from "../PostItem/PostItem";
 import PostItemList from "../PostItemList";
@@ -19,11 +20,17 @@ const initialPosts: Post[] = [
   },
 ];
 export default function PostsContainer() {
-
   const [posts, setPosts] = useState<Post[]>(initialPosts);
 
   return (
     <div className="container">
+      <Helmet>
+        <title>Blog Feed</title>
+        <meta 
+          name="description"
+          content="Blog news feed"
+        />
+      </Helmet>
       <PostForm 
         onPosts={posts}
         onSetPosts={(posts: Post[]) => setPosts(posts)}
