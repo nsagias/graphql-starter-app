aabef68380c474505befb0bdcceed81a96f0d4c5
Nick <nsagias@gmail.com>
2023-05-10 18:49:01 -0400

    move code to src folder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import path from 'path';
 
 const app = express();
+
 const root = path.join(__dirname, "../../");
 
 app.use("/", express.static(path.join(root, "/frontend/build")));
