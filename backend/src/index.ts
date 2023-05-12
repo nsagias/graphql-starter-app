06d053c4abcb28e44deda3fddc9ef8c835618b55
Nick <nsagias@gmail.com>
2023-05-12 01:42:40 -0400

    refactor and add notes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,85 +9,67 @@ import bodyParser from 'body-parser';
 import helmet from "helmet";
 import compress from "compression";
 import path from "path";
+import typeDefs from "./services/schema";
+import resolvers from "./services/resolvers";
 
 
 interface MyContext {
   token?: string;
 }
 
-const typeDefs = `
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
 const root = path.join(__dirname, "../../");
+
 const app = express();
+
 app.use(compress());
 app.use(cors());
 
 const httpServer = http.createServer(app);
 
+// Async Start of Server
 const startServer = async () => {
-const server = new ApolloServer<MyContext>({
-  schema: makeExecutableSchema({
-    typeDefs,
-    resolvers,
-  }),
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-});
+  
+  // Create Apollo Servier
+  const server = await new ApolloServer<MyContext>({
+    schema: makeExecutableSchema({
+      typeDefs,
+      resolvers
+    }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-await server.start();
+  await server.start();
 
-app.use(
-  "/graphql",
-    cors<cors.CorsRequest>(),
-    bodyParser.json(),
-    expressMiddleware(server, {
-      context: async ({ req }) => ({ token: req.headers.token }),
-  }),
-);
+  //  Apollo endpoint
+  app.use(
+    "/graphql",
+      cors<cors.CorsRequest>(),
+      bodyParser.json(),
+      expressMiddleware(server, {
+        context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+  );
 
-app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self "],
-    scriptSrc: ["'self'", "'unsafe-inline'"],
-    styleSrc: ["'self'", "'unsafe-inline'"],
-    imgSrc: ["'self'", "data:", "*.amazonaws.com"]
-  }
-}));
-app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+  // Helmet Section
+  app.use(helmet());
+  app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self "],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+    }
+  }));
+  app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
-app.use("/", express.static(path.join(root, "/frontend/build")));
-app.use("/uploads", express.static(path.join(root, "uploads")));
+  // React Server Side Rendering Section
+  app.use("/", express.static(path.join(root, "/frontend/build")));
+  app.use("/uploads", express.static(path.join(root, "uploads")));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(root, "frontend/build/index.html"));
+  });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(root, "frontend/build/index.html"));
-});
-
-
+  // Create server
   await new Promise<void>((resolve) => httpServer.listen({ port: 8000 }, resolve));
   console.log(`🚀 Server ready at http://localhost:8000/`);
 }
