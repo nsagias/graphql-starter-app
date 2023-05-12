ef287cf29cb16cb422abcdbedff6a2165272f3ce
Nick <nsagias@gmail.com>
2023-05-12 16:34:24 -0400

    create model index

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,10 +1,11 @@
 import { Sequelize, Options } from "sequelize";
+import models from "../../models";
 
 import configFile from "../config/index"
 
 const env = process.env.NODE_ENV || "development";
 const config = (configFile as {[key: string]: Options})[env];
-const db: Sequelize = new Sequelize({
+const sequelize: Sequelize = new Sequelize({
   ...config,
   define: {
     underscored: true
@@ -12,11 +13,15 @@ const db: Sequelize = new Sequelize({
 });
 
 try {
-  db.authenticate();
+  sequelize.authenticate();
   console.log("DB SUCCESS")
 } catch (error) {
   console.log("DB FAILED");
 };
 
+const db = {
+  models: models,
+  sequelize,
+}
 
 export default db;
