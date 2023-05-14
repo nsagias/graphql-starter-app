da86cb86aaf05eb189c6db4e23a5ecc2ff21691d
Nick <nsagias@gmail.com>
2023-05-14 19:21:22 -0400

    wrap in try catch

--- backend/src/services/resolvers.ts
+++ backend/src/services/resolvers.ts
@@ -5,33 +5,43 @@ import { Post, User } from "../../models";
 
 const resolvers = {
     Post: {
-      user(post: { getUser: () => any; }, args: any, context: any ) {
-        return post.getUser();
+      async user(post: { getUser: () => any; }, args: any, context: any ) {
+        try {
+          return post.getUser();
+          } catch (error) {
+            console.log(error);
+        }
       }
     },
     RootQuery: {
         async posts(root: any, args: any, context: any) {
-          const posts = await Post.findAll({
-            order: [["createdAt", "DESC"]]
-          });
-          console.log("posts", posts);
-          return posts;
+          try {
+            const posts = await Post.findAll({
+              order: [["createdAt", "DESC"]]
+            });
+            console.log("posts", posts);
+            return posts;
+          } catch (error) {
+            console.log(error);
+          }
         },
     },
     RootMutation: {
         // async addPost(root: any, { post, user }: any, context: any) {
         async addPost(root: any, { post }: any, context: any) {
-
-          // Get all users
-          const users = await User.findAll();
-          // manually assign user from userlist // dev
-          const usersRow = users[0];
-
-          const newPost = await Post.create({ ...post });
-          newPost.setUser(usersRow.id);
-          // logger.log({ level: 'info', message: 'Post was created' });
-          return newPost;
-          
+          try {
+            // Get all users
+            const users = await User.findAll();
+            // manually assign user from userlist // dev
+            const usersRow = users[0];
+            
+            const newPost = await Post.create({ ...post });
+            newPost.setUser(usersRow.id);
+            // logger.log({ level: 'info', message: 'Post was created' });
+            return newPost;
+            } catch (error) {
+              console.log(error);
+          }   
         },
     },
 };
