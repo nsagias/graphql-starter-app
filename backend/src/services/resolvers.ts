1c429e4defd63857e0fd7aa7a6917cadf325e6cf
Nick <nsagias@gmail.com>
2023-05-14 19:05:30 -0400

    add get user for post and update addPost mutation

--- backend/src/services/resolvers.ts
+++ backend/src/services/resolvers.ts
@@ -1,9 +1,14 @@
 // import logger from '../../helpers/logger';
 
-import { Post } from "../../models";
+import { Post, User } from "../../models";
 
 
 const resolvers = {
+    Post: {
+      user(post: { getUser: () => any; }, args: any, context: any ) {
+        return post.getUser();
+      }
+    },
     RootQuery: {
         async posts(root: any, args: any, context: any) {
           const posts = await Post.findAll({
@@ -16,10 +21,16 @@ const resolvers = {
     RootMutation: {
         // async addPost(root: any, { post, user }: any, context: any) {
         async addPost(root: any, { post }: any, context: any) {
-        
+
+          // Get all users
+          const users = await User.findAll();
+          // manually assign user from userlist // dev
+          const usersRow = users[0];
+
+          const newPost = await Post.create({ ...post });
+          newPost.setUser(usersRow.id);
           // logger.log({ level: 'info', message: 'Post was created' });
-          const newPostObject = await Post.create({ text: post.text });
-          return newPostObject;
+          return newPost;
           
         },
     },
