71f5f774c2a04a56595f347c148b89818b1e19e4
Nick <nsagias@gmail.com>
2023-05-14 20:50:08 -0400

    add apollo to frontend

--- frontend/src/apollo/index.ts
+++ frontend/src/apollo/index.ts
@@ -1,17 +1,35 @@
-import { ApolloClient, InMemoryCache, from, HttpLink } from '@apollo/client';
-
+import { ApolloClient, InMemoryCache, from, HttpLink, gql } from '@apollo/client';
 import { onError } from "@apollo/client/link/error";
 
-const client = new ApolloClient({ link: from([
+const client = new ApolloClient({
+  link: from([
     onError(({ graphQLErrors, networkError }) => {
       if (graphQLErrors) {
         graphQLErrors.map(({ message, locations, path }) => console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`));
         if (networkError) console.log(`[Network error]: ${networkError}`);
       }
     }),
-    new HttpLink({ uri: 'http://localhost:8000/graphql' }),
-  ]),
-  cache: new InMemoryCache(),
+    new HttpLink({ uri: 'http://localhost:8000/graphql'}),
+]),
+
+cache: new InMemoryCache(),
+
 });
 
+// text connection 
+// client.query({
+//   query: gql`
+//     {
+//       posts {
+//         id
+//         text
+//         user {
+//           avatar
+//           username
+//         }
+//       }
+//     }`
+// }).then(result => console.log(result));
+
 export default client;
+
