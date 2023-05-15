9181fdd1c0dabd81f9462870ffa305906bffb8d1
Nick <nsagias@gmail.com>
2023-05-14 23:40:32 -0400

    update component with correct data format

--- frontend/src/components/PostItem/index.tsx
+++ frontend/src/components/PostItem/index.tsx
@@ -1,13 +1,13 @@
 import "./PostItem.css";
 import { PostItemProps } from "./PostItem";
 
-
 export default function PostItem({ post }: PostItemProps ): JSX.Element {
+
   return (
     <div key={post && post.id} className="post">
       <div className="header">
-        <img src={post && post.avatar} />
-        <h2>{post && post.username}</h2>
+        <img src={post && post.user.avatar} />
+        <h2>{post && post.user.username}</h2>
       </div>
       <p className="content">
         {post && post.text}
