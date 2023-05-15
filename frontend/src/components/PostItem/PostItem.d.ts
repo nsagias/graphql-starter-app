7e525bd5e05c1c33d463d683c63132ae33584739
Nick <nsagias@gmail.com>
2023-05-14 23:39:46 -0400

    update data shape

--- frontend/src/components/PostItem/PostItem.d.ts
+++ frontend/src/components/PostItem/PostItem.d.ts
@@ -1,9 +1,20 @@
+// console.log("DATA POST [0].USER.AVATAR", data && data.posts[0].user.avatar);
+// console.log("DATA POST [0].USER.USERNAME", data && data.posts[0].user.username);
+// console.log("DATA POST [0].id", data && data.posts[0].id);
+// console.log("DATA POST [0].text", data && data.posts[0].text);
+
+export type PostUserData = {
+  avatar: string,
+  username: string,
+}
+
 export type Post = {
   id: number,
   text: string,
-  avatar: string,
-  username: string,
+  user: PostUserData
 };
+
+
 
 export type PostItemProps = {
   post?: Post | undefined
