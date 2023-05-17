70596f567622acdd8d7df2471ae8bd68cb6d9743
Nick <nsagias@gmail.com>
2023-05-16 23:29:11 -0400

    remove dead code

--- frontend/src/components/PostForm/index.tsx
+++ frontend/src/components/PostForm/index.tsx
@@ -34,36 +34,9 @@ export default function PostForm({ onPosts, onSetPosts }: PostFormProps ): JSX.E
     refetchQueries: [{query: GET_POSTS}]
   });
 
-  // const [addPost] = useMutation(ADD_POST, {
-  //   update(cache, { data: { addPost } }) {
-  //     const data: Posts | null = cache.readQuery({ query: GET_POSTS });
-  //     if ( data && data.length) {
-  //       const newData = { posts: [addPost, ...data.posts]};
-  //       cache.writeQuery({ query: GET_POSTS, data: newData });
-  //     }
-  //   }
-  // });
-
   const handleSubmitPost = (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
-    
+    e.preventDefault();    
     addPost({ variables: { post: { text: postContent }}});
-    // addPost({ variables: { post: { text: postContent },
-    //   optimisticResponse: {
-    //     addPost: {
-    //       __typename: "Post",
-    //       text: postContent,
-    //       id: -1, 
-    //       user: {
-    //         __typeName: "User",
-    //         username: "Loading...",
-    //         avatar: "/public/loading.gif"
-    //       }
-    //     }
-    //   }
-    // }});
-
-
     setPostContent("");
   };
 
